2b94c1d99418b6a1371d8a9b7c5a423c3c694037
masterwali <walimaster0131@yahoo.com>
2019-01-26 13:12:07 -0500

    Create TrainerServices.swift

---
+++ TrainerServices.swift
@@ -0,0 +1,44 @@
+//
+//  TrainerServices.swift
+//
+//  Created by Waleed on 1/22/19.
+//  Copyright © 2019 Waleed Sarwari. All rights reserved.
+//
+
+import Foundation
+
+final class TrainerServices {
+    
+    // MARK: - Properties
+    
+    static let shared: TrainerServices = TrainerServices()
+    
+    private init() { }
+    
+    // Save sports values
+    func saveSportsValues(sports: Array<String>) {
+        // Update db with new value
+        let newValue = [
+            Trainer.UserInfoKey.sports : sports
+            ] as [String : Any]
+        
+        TRAINER_DB_REF.child(Utilities.getCurrentUserId()).updateChildValues(newValue)
+
+    }
+    
+    func getSportsValues(completionHandler: @escaping(Array<String>) -> Void) {
+        TRAINER_DB_REF.child(Utilities.getCurrentUserId()).observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            var sportsValues: Array<String>
+            
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            sportsValues = value?[Trainer.UserInfoKey.sports] as? Array<String> ?? [""]
+            
+            completionHandler(sportsValues)
+            // ...
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+}
